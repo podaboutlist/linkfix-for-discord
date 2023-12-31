78877f3f039d13b7c9b2ac42e3a2aace7c1efa06
Ralph Drake <rama.o.ralph@gmail.com>
2023-12-30 22:56:01 -0500

    refactor: move @types up one directory

--- src/@types/Discord.d.ts
+++ src/@types/Discord.d.ts
@@ -1,11 +1,9 @@
 import { Collection } from "discord.js";
 import { CustomCommand } from "./CustomCommand";
-import { Pool } from "pg";
 
 // extend discord.js Client type to allow commands collection
 declare module "discord.js" {
   export interface Client {
     commands: Collection<string, CustomCommand>;
-    pgPool: Pool;
   }
 }
