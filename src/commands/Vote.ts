35d486927645eff2edbf0ad3255a184c33dc971f
shoegaze <7885170+shoegaze@users.noreply.github.com>
2024-05-22 01:41:51 +0900

    Formats with prettier

--- src/commands/Vote.ts
+++ src/commands/Vote.ts
@@ -19,9 +19,7 @@ export function createVoteCommand(): CustomCommand {
   }
 
   return {
-    data: new SlashCommandBuilder()
-        .setName("vote")
-        .setDescription(description),
+    data: new SlashCommandBuilder().setName("vote").setDescription(description),
     execute: async (interaction: CommandInteraction) => {
       await interaction.reply({
         content,
